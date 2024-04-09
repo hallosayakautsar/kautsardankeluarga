08328e7f8f1a94fb8f9af44310d66c2e75cd1ce7
hallosayakautsar <65478094+hallosayakautsar@users.noreply.github.com>
2024-04-09 17:52:50 +0700

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,69 @@
+function animate(typewriter) {
+    sequence = [
+        {
+            text: "root@kautsar:~# ",
+            instant: true,
+            delayAfter: 350
+        },
+        {
+            text: "Assalamu'alaikum Warrahmatullahi Wabarakatuh,\n",
+            delayAfter: 350
+        },
+        {
+            text: "root@kautsar:~# ",
+            instant: true,
+            delayAfter: 350
+        },
+        {
+            text: "Selamat Hari Raya Idul Fitri 1445 Hijriah, saudaraku.\n",
+            delayAfter: 350
+        },
+        {
+            text: "root@kautsar:~# ",
+            instant: true,
+            delayAfter: 350
+        },
+        {
+            text: "Saya Kautsar beserta Keluarga, di hari yang suci dan penuh berkah ini, apabila saya pernah berbuat salah baik dari perilaku maupun lisan\n",
+            delayAfter: 350
+        },
+        {
+            text: "root@kautsar:~# ",
+            instant: true,
+            delayAfter: 350
+        },
+        {
+            text: "maka ingatlah bahwa itu semua... MEMANGLAH SAYA SENGAJA\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\badalah benar-benar kekhilafan saya.\n",
+            delayAfter: 350
+        },
+        {
+            text: "root@kautsar:~# ",
+            instant: true,
+            delayAfter: 350
+        },
+        {
+            text: "Oleh karena itu, saya atas nama pribadi beserta keluarga ingin meminta maaf sebesar-besarnya atas segala kekhilafan saya.\n",
+            delayAfter: 350
+        },
+        {
+            text: "root@kautsar:~# ",
+            instant: true,
+            delayAfter: 350
+        },
+        {
+            text: "Minal Aidzin Wal Faidzin.\n",
+            delayAfter: 350
+        },
+        {
+            text: "root@kautsar:~# ",
+            instant: true,
+            delayAfter: 350
+        },
+        {
+            text: "Wassalamu'alaikum Warrahmatullahi Wabarakatuh.\n",
+            delayAfter: 350
+        },
+    ];
+
+    typewriter.playSequence(sequence);
+}
